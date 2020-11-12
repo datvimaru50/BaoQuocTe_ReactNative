e551e5fbf2bda644e5c640a7d88f88c50628d8e7
dat vu quoc <datvimaru50@gmail.com>
2020-11-12 17:49:20 +0700

    update homepage

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+import ScrollableTabView, { ScrollableTabBar }  from "react-native-scrollable-tab-view";
+
 
 const ICON_SIZE = {width: 24, height: 24};
 const GUTTER = 10;
@@ -53,6 +57,21 @@ const slides = [
   {
     img: "https://baoquocte.vn/stores/news_dataimages/huynguyen/112020/06/16/croped/thu-tuong-nguyen-xuan-phuc-dien-dam-voi-thu-tuong-thai-lan.jpg",
     text: "Thủ tướng Nguyễn Xuân Phúc điện đàm với Thủ tướng Thái Lan"
+  },
+]
+
+const slides3 = [
+  {
+    img: "https://baoquocte.vn/stores/news_dataimages/huynguyen/112020/06/16/croped/tong-thu-ky-lien-hop-quoc-bo-truong-ngoai-giao-sri-lanka-gui-dien-tham-hoi-ve-tinh-hinh-mua-lu-o-mien-trung-viet-nam.jpg",
+    text: "Tổng Thư ký Liên hợp quốc, Bộ trưởng Ngoại giao Sri Lanka gửi điện thăm hỏi về tình hình mưa lũ ở miền Trung",
+  },
+  {
+    img: "https://baoquocte.vn/stores/news_dataimages/tuananh/112020/07/21/thu-tuong-nguyen-xuan-phuc-gap-mat-cac-truong-co-quan-dai-dien-viet-nam-o-nuoc-ngoai-nhiem-ky-2020-2023_9.jpg?rt=20201107215718",
+    text: "Thủ tướng Nguyễn Xuân Phúc gặp mặt các Trưởng cơ quan đại diện Việt Nam ở nước ngoài nhiệm kỳ 2020-2023"
+  },
+  {
+    img: "https://baoquocte.vn/stores/news_dataimages/tuananh/112020/05/15/viet-nam-australia-ky-chuong-trinh-hanh-dong-trien-khai-quan-he-doi-tac-chien-luoc-giai-doan-2020-2023.jpg?rt=20201105155820",
+    text: "Việt Nam-Australia ký Chương trình hành động triển khai quan hệ Đối tác Chiến lược giai đoạn 2020-2023"
   },
 ]
 
@@ -185,23 +204,7 @@ const App: () => React$Node = () => {
             {/* ***************************
                       QUỐC TẾ TV
             ******************************/}
-            <View style={{padding: 10, backgroundColor: "#f1f1f1", marginTop: 15}}>
-              <Image 
-                style={{width: 97, height: "auto", aspectRatio: 300/74}}
-                source={require("./assets/icons/logo-tv.png")}
-              />
-              <View style={{width: "100%", aspectRatio: 16/9, position: "relative", marginVertical: 10}}>
-                <Image 
-                  style={{width: "100%", height: "100%"}}
-                  source={{uri: "https://baoquocte.vn/stores/video_data/dangtuan/102020/30/21/medium/3602_unnamed_edit.png"}}
-                />
-                <Image 
-                  style={{width: 69, height: "auto", aspectRatio: 1, position: "absolute", top: "50%", left: "50%", marginTop: -34.5, marginLeft: -34.5}}
-                  source={require("./assets/icons/ico-play-x.png")}
-                />
-              </View>
-              <Text style={{fontSize: 18}}>TRỰC TUYẾN: Con đường Ngoại giao - MOFA Open Day 2020</Text>
-            </View>
+            <VideoLayout imageTitleIcon />
 
             {/* ***************************
                        TIN NÓNG 2
@@ -217,7 +220,7 @@ const App: () => React$Node = () => {
                         style={{width: "40%", aspectRatio: 16/9, marginRight: 10}}
                         source={{uri: item.img}}
                       />
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333"}}>{item.text}</Text>
+                      <Text style={styles.newsTitle}>{item.text}</Text>
                     </View>
                   )
                 }
@@ -228,83 +231,46 @@ const App: () => React$Node = () => {
             {/* ***************************
                        TIN MỚI
             ******************************/}
-            <View style={{padding: 10, marginTop: 10, backgroundColor: "#e2e3e5"}}>
-              <View style={{width: "100%", height: 250, alignItems: "center"}}>
-                <Image 
-                  style={{width: 300, height: 250}}
-                  source={{uri: "https://baoquocte.vn/stores/ads_data/tuananh/042017/03/16/160514_PBM_300-250_Final.jpg"}}
-                />
-              </View>
+            <View style={styles.sectionContainer}>
 
-              <View style={{padding: 10, marginTop: 10, backgroundColor: "#fff",}}>
-                  <View style={{backgroundColor: '#f6891a', padding: 5, marginBottom: 10, alignSelf: 'flex-start' }}>
-                    <Text style={{color: '#ffffff', textTransform: "uppercase", fontWeight: "bold"}}>
+              <Banner300x250 />
+
+              <View style={{...styles.sectionContainer, backgroundColor: "#fff"}}>
+                  <View style={styles.tinMoititleContainer}>
+                    <Text style={styles.tinMoiTitle}>
                       Tin mới
                     </Text>
                   </View>
 
                   <ScrollView style={{width: "100%", height: 300}} nestedScrollEnabled>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
-                    <View style={{flexDirection: "row", paddingBottom: 10, marginBottom: 10, borderBottomColor: "#ddd", borderBottomWidth: 1, borderStyle: "dotted"}}>
-                      <Text style={{color: "#999"}}>16:00</Text>
-                      <Text style={{flexShrink: 1, fontSize: 16, color: "#333", marginLeft: 5}}>Căng thẳng thương mại Mỹ-Trung thời tân Tổng thống Mỹ: Mong manh nhưng ổn định hơn, liệu có tiếp tục ‘ngừng bắn’?</Text>
-                    </View>
+                    {
+                      slides.map((item, idx)=>(
+                        <View key={idx} style={styles.tinMoiItemContainer}>
+                          <Text style={{color: "#999", marginRight: 5}}>16:00</Text>
+                          <Text style={{...styles.newsTitle, fontWeight: "normal"}}>{item.text}</Text>
+                        </View>
+                      ))
+                    }
                   </ScrollView>
               </View>
             </View>
+            
+            <CategoryLayout1 mainTitle="Phân tích chuyện thời sự" />
 
-            
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
+            <CategoryTabScroll mainTitle="Ngoại giao" />
+
+            <CategoryTabScroll mainTitle="Thế giới" />
+
+            <CategoryLayout1 mainTitle="Hồ sơ" />
+
+            <VideoLayout mainTitle="Video" />
+
+            <View style={{...styles.sectionContainer, flexDirection: "row", justifyContent: "space-between"}}>
+              <CategoryLayout2 mainTitle="Thời sự" width="48%" />
+              <CategoryLayout2 mainTitle="Xã hội" width="48%" />
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            
-            <LearnMoreLinks />
+
+
 
 
           </View>
@@ -314,7 +280,242 @@ const App: () => React$Node = () => {
   );
 };
 
+function VideoLayout(props){
+  /* 
+    FAKE api: api.baoquocte.vn/categoryID=`${props.categoryID}`
+  */
+  return <View style={{padding: 10, backgroundColor: "#f1f1f1", marginTop: 15}}>
+            {
+              props.imageTitleIcon && 
+              <Image 
+                style={{width: 97, height: "auto", aspectRatio: 300/74, marginBottom: 10}}
+                source={require("./assets/icons/logo-tv.png")}
+              />
+            }
+
+            {
+              props.mainTitle &&
+              <Text style={styles.mainCateTitle}>{props.mainTitle}</Text>
+            }
+
+            <View style={{width: "100%", aspectRatio: 16/9, position: "relative", marginBottom: 10}}>
+              <Image 
+                style={{width: "100%", height: "100%"}}
+                source={{uri: "https://baoquocte.vn/stores/video_data/dangtuan/102020/30/21/medium/3602_unnamed_edit.png"}}
+              />
+              <Image 
+                style={{width: 69, height: "auto", aspectRatio: 1, position: "absolute", top: "50%", left: "50%", marginTop: -34.5, marginLeft: -34.5}}
+                source={require("./assets/icons/ico-play-x.png")}
+              />
+            </View>
+            <Text style={{fontSize: 18}}>TRỰC TUYẾN: Con đường Ngoại giao - MOFA Open Day 2020</Text>
+          </View>
+}
+
+function Banner300x250(){
+  return <View style={styles.banner300x200Containner}>
+          <Image 
+            style={{width: 300, height: 250}}
+            source={{uri: "https://baoquocte.vn/stores/ads_data/tuananh/042017/03/16/160514_PBM_300-250_Final.jpg"}}
+          />
+        </View>
+}
+
+function CategoryLayout1(props) {
+  return <View style={{...styles.sectionContainer, backgroundColor: "#fff", ...props.styles}}>
+          <Text style={styles.mainCateTitle}>{props.mainTitle}</Text>
+          {
+            slides3.map((item, idx)=>{
+              const isFirst = idx === 0;
+              return(
+
+                <View 
+                  key={idx} 
+                  style={{
+                    flexDirection: "row", 
+                    justifyContent: "space-between", 
+                    marginTop: isFirst ? 0 : 10, 
+                    paddingTop: isFirst ? 0 : 10, 
+                    borderTopColor: isFirst ? "transparent" : "#ddd", 
+                    borderTopWidth: isFirst ? 0 : 1,
+                    position: "relative",
+                    paddingLeft: isFirst ? 0 : 15,
+                  }}>
+                  <Image 
+                    style={{width: "40%", aspectRatio: 16/9, display: isFirst ? "flex" : "none"}}
+                    source={{uri: item.img}}
+                  />
+                  <View style={{width: isFirst ? "57%" : "100%"}}>
+                    <Text style={{...styles.newsTitle, fontWeight: isFirst ? "bold" : "normal"}}>{item.text}</Text>
+                  </View>
+
+                  {
+                    !isFirst &&
+                    <BlueBullet />
+                  }
+                </View>
+              )
+            })
+          }
+        </View>
+}
+
+function CategoryLayout2(props) {
+  return (
+    <View style={{width: props.width,}}>
+      <Text style={styles.mainCateTitle}>{props.mainTitle}</Text>
+
+      {
+        slides3.map((item, idx)=>{
+          const isFirst = idx === 0;
+          const styleNormal = {...styles.newsTitle, padding: 10, borderTopWidth: 1, borderTopColor: "#ddd", backgroundColor: "#fff"};
+          const styleFirst = {...styles.newsTitle, padding: 10, fontWeight: "bold", backgroundColor: "#fff"};
+          return(
+
+            <View 
+              key={idx} 
+            >
+
+              <Image 
+                style={{...styles.imgWide, display: isFirst ? "flex" : "none",}}
+                source={{uri: item.img}}
+              />
+              
+              <Text style={isFirst ? styleFirst : styleNormal}>
+                {item.text}
+              </Text>
+            </View>
+          )
+        })
+      }
+    </View>
+  );
+}
+
+function CategoryTabScroll(props) {
+  let [active, setActive] = useState(0);
+
+  function handleChangeTab(tab){
+    setActive(tab.i);
+  }
+  return (
+    <View style={{...styles.sectionContainer, paddingTop: 0}}>
+      <ScrollableTabView
+        style={{ marginBottom: 10 }}
+        initialPage={0}
+        onChangeTab={handleChangeTab}
+        renderTabBar={() => <ScrollableTabBar />}
+      >
+        <Text tabLabel={props.mainTitle.toUpperCase()} />
+        <Text tabLabel='Tin bộ ngoại giao' />
+        <Text tabLabel='Bảo hộ công dân' />
+        <Text tabLabel='Thường thức ngoại giao' />
+      </ScrollableTabView>
+
+      <View style={{backgroundColor: "#fff"}}>
+        {
+          slides3.map((item, idx)=>{
+            const isFirst = idx === 0;
+            const style169 = {position: "relative", width: "100%", aspectRatio: 16/9};
+            const styleDefault = {position: "relative", width: "100%"};
+
+            const styleNormal = {...styles.newsTitle, paddingVertical: 10, borderTopWidth: 1, borderTopColor: "#ddd"};
+            return(
+
+              <View 
+                key={idx} 
+                style={{
+                  position: "relative",
+                  paddingLeft: isFirst ? 0 : 20,
+                }}>
+
+                <View style={isFirst ? style169 : styleDefault}>
+                  <Image 
+                    style={{...styles.imgWide, display: isFirst ? "flex" : "none",}}
+                    source={{uri: item.img}}
+                  />
+                  
+                  <Text 
+                    style={isFirst ? styles.slideText : styleNormal}
+                  >
+                    {item.text}
+                  </Text>
+                </View>
+
+                {
+                  !isFirst && 
+                  <Image 
+                    width={7}
+                    aspectRatio={1}
+                    style={styles.blueBullet}
+                    source={require("./assets/icons/bl-square-blue.png")}
+                  />
+                  
+                }
+              </View>
+            )
+          })
+        }
+
+        <View style={styles.moreTextContainer}>
+          <Text style={styles.moreText}>{"Xem thêm >>"}</Text>
+        </View>
+
+      </View>
+    </View>
+  );
+}
+
+function BlueBullet(){
+  return <Image 
+            width={7}
+            height={7}
+            style={{position: "absolute", top: 20, left: 0}}
+            source={require("./assets/icons/bl-square-blue.png")}
+          />
+}
+
+/* =====================
+        STYLESHEET
+  ======================
+*/
 const styles = StyleSheet.create({
+  imgWide: {
+    width: "100%",
+    aspectRatio: 16/9
+  },
+  newsTitle: {
+    flexShrink: 1, 
+    fontSize: 16, 
+    color: "#333"
+  },
+  newsDesc: {
+    flexShrink: 1, 
+    fontSize: 14,
+  },
+  blueBullet: {
+    position: "absolute", 
+    top: 20, 
+    left: 7,
+  },
+  moreTextContainer: {
+    paddingHorizontal: 20, 
+    paddingBottom: 10, 
+    alignItems: "flex-end"
+  },
+  moreText: {
+    color: "#0066b3", 
+    fontSize: 12, 
+    fontWeight: "bold", 
+    textTransform: "uppercase"
+  },
+  mainCateTitle:{
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontFamily: "Times New Roman",
+    fontSize: 16,
+    marginBottom: 10,
+  },
   topicBar: {
     flexDirection: "row", 
     alignItems: "center", 
@@ -350,6 +551,30 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     backgroundColor: "#00000066",
   },
+  banner300x200Containner: {
+    width: "100%", 
+    height: 250, 
+    alignItems: "center"
+  },
+  tinMoititleContainer: {
+    backgroundColor: '#f6891a', 
+    padding: 5, 
+    marginBottom: 10, 
+    alignSelf: 'flex-start' 
+  },
+  tinMoiTitle: {
+    color: '#ffffff', 
+    textTransform: "uppercase", 
+    fontWeight: "bold"
+  },
+  tinMoiItemContainer: {
+    flexDirection: "row", 
+    paddingBottom: 10, 
+    marginBottom: 10, 
+    borderBottomColor: "#ddd", 
+    borderBottomWidth: 1, 
+    borderStyle: "dotted"
+  },
 
   // ****************
   scrollView: {
@@ -363,8 +588,9 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
   },
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: "#e2e3e5",
   },
   sectionTitle: {
     fontSize: 24,
